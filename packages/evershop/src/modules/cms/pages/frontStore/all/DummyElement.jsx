613eca97743dd4d860466a65fd13f8134d39e472
suryr <suryr@paypal.com>
2024-02-10 22:23:10 +0530

    Homepage change

---
+++ packages/evershop/src/modules/cms/pages/frontStore/all/DummyElement.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './DummyElement.scss';
+
+export default function Dummy() {
+  return (
+    <div className="dummy" />
+  );
+}
+
+export const layout = {
+  areaId: 'header',
+  sortOrder: 4
+};
