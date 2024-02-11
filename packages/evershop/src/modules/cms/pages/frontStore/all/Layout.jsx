4bf87ed75b40939efb754174832c8ec149a98f1e
suryr <suryr@paypal.com>
2024-02-11 11:16:43 +0530

    whatsapp

--- packages/evershop/src/modules/cms/pages/frontStore/all/Layout.jsx
+++ packages/evershop/src/modules/cms/pages/frontStore/all/Layout.jsx
@@ -4,8 +4,12 @@ import LoadingBar from '@components/common/LoadingBar';
 import './Layout.scss';
 import './tailwind.scss';
 
-
 export default function Layout() {
+  // Define your message variable
+  const whatsappMessage = "Hello! I was browsing through Krystal Touch and I need some help!";
+
+  // Encode the message for URL
+  const encodedMessage = encodeURIComponent(whatsappMessage);
 
   return (
     <>
@@ -32,6 +36,19 @@ export default function Layout() {
       <div className="footer">
         <Area id="footer" noOuter coreComponents={[]} />
       </div>
+
+      {/* Font Awesome CSS */}
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+
+      {/* WhatsApp icon with dynamic message */}
+      <a
+        href={`https://wa.me/918940882711?text=${encodedMessage}`}
+        className="whatsapp_float" // Using className instead of class for JSX
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <i className="fa fa-whatsapp whatsapp-icon"></i>
+      </a>
     </>
   );
 }
